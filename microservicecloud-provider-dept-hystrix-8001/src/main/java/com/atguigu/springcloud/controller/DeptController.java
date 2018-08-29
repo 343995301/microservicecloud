3db590c64ee79b3b1d5805199dbb12261ee22973
luxin <343995301@qq.com>
2018-08-29 15:09:23 +0800

    ddd

--- microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -10,6 +10,8 @@ import com.atguigu.springcloud.entities.Dept;
 import com.atguigu.springcloud.service.DeptService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+import java.util.List;
+
 @RestController
 public class DeptController
 {
@@ -18,22 +20,35 @@ public class DeptController
 
 	@RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
 	//一旦调用服务方法失败并抛出了错误信息后，会自动调用@HystrixCommand标注好的fallbackMethod调用类中的指定方法
-	@HystrixCommand(fallbackMethod = "processHystrix_Get")
+	@HystrixCommand(fallbackMethod = "test")
 	public Dept get(@PathVariable("id") Long id)
 	{
 
 		Dept dept = this.service.get(id);
-		
-		if (null == dept) {
+		System.out.println(dept);
+
+		if (id>10) {
 			throw new RuntimeException("该ID：" + id + "没有没有对应的信息");
 		}
-		
+
 		return dept;
 	}
 
-	public Dept processHystrix_Get(@PathVariable("id") Long id)
+//	public Dept processHystrix_Get(@PathVariable("id") Long id)
+//	{
+//		return new Dept().setDeptno(id).setDname("该ID：" + id + "没有没有对应的信息,null--@HystrixCommand")
+//				.setDb_source("no this database in MySQL");
+//	}
+	public Dept test(@PathVariable("id") Long id){
+		Dept d= new Dept();
+		d.setDname("熔断错误");
+		d.setDeptno(id);
+		d.setDb_source("kong");
+		return d;
+	}
+	@RequestMapping(value = "/dept/list", method = RequestMethod.GET)
+	public List<Dept> list()
 	{
-		return new Dept().setDeptno(id).setDname("该ID：" + id + "没有没有对应的信息,null--@HystrixCommand")
-				.setDb_source("no this database in MySQL");
+		return service.list();
 	}
 }
