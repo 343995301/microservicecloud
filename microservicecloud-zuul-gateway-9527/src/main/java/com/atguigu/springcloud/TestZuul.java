f6c12e1d39d3d13b0740bdc3fe5b05fbe24f0744
luxin <343995301@qq.com>
2018-08-29 15:11:57 +0800

    ddd

---
+++ microservicecloud-zuul-gateway-9527/src/main/java/com/atguigu/springcloud/TestZuul.java
@@ -0,0 +1,29 @@
+package com.atguigu.springcloud;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+public class TestZuul extends ZuulFilter{
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+
+        return false;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+//        ctx.put(FilterConstants.REQUEST_URI_KEY, modifiedRequestPath);
+        return null;
+    }
+}
